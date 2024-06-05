3058a9324fb9c5143ca27313b97c745299178ab5
Leroy <leroy.tang.zw@gmail.com>
2024-06-05 23:43:16 +0800

    updated Action,AdminDetails,ContactPage,DetailsPage and HomePage

---
+++ src/views/ContactPage.js
@@ -0,0 +1,27 @@
+import donaldDuck from '../images/donald-duck-bowing-hfuppyabg138fxq7.gif';
+import '../ContactAdmin.css';
+
+function App () {
+    return (
+        <>
+      <div className="App">
+        <header className="App-header">
+        <h1>
+            Thank you!
+        </h1>
+          <img src={donaldDuck} className="App-logo" alt="logo" />
+          <p>
+            Call Bob: +65 9011 1100 </p>
+            <p>
+            or  
+            </p>  
+            <p>
+            Email Bob: hellobob@vision.sg 
+            </p> 
+            </header> 
+            </div> 
+            </>
+        ); 
+    } 
+    
+    export default App;
